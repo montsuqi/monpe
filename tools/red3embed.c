8ffb04058aa9df40cca48885f8cb022bee794e60
yusuke mihara <mihara@netlab.jp>
2011-04-18 07:42:00 +0000

    ヘルプの修正

--- tools/red3embed.c
+++ tools/red3embed.c
@@ -414,7 +414,7 @@ int main(int argc, char *argv[])
   GError *error = NULL;
   GOptionContext *ctx;
 
-  ctx = g_option_context_new("- embed data to .dia");
+  ctx = g_option_context_new("<.red3> <.rec> <.dat>");
   g_option_context_add_main_entries(ctx, entries, NULL);
   if (!g_option_context_parse(ctx,&argc,&argv,&error)) {
     g_print("option parsing failed:%s\n",error->message);
